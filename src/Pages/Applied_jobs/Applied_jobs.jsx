cf385735de975f1e095b2a50cceba274d1a7ba19
Rifazul <rifazul50@gmail.com>
2023-11-07 16:10:55 +0600

    add a print document system

--- src/Pages/Applied_jobs/Applied_jobs.jsx
+++ src/Pages/Applied_jobs/Applied_jobs.jsx
@@ -1,12 +1,15 @@
 
-import { useEffect, useState } from "react";
+import { useEffect,  useRef,  useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import CardTable from "./CardTable";
 import PageTitle from "../../components/Shared/PageTitle/PageTitle";
 import useAxios from "../../hooks/useAxios";
+import { useReactToPrint } from "react-to-print";
 
 
 const Applied_jobs = () => {
+  
+
 const[applies,setApplies] = useState([]) ;
 const {user}= useAuth()  
 const axiosSecure = useAxios();
@@ -17,69 +20,85 @@ useEffect(()=>{
  })
 },[axiosSecure,user?.email])
 
+// print system empiliment
+const componentRef = useRef()
+const handlerPrint = useReactToPrint({
+  content: ()=> componentRef.current,
+  documentTitle:'emp-data.png',
+  
+});
+
+
+
+
 
     return (
-        <div>
-            <PageTitle  title="Applied jobs Page"/>
+     <>
+        <div ref={componentRef} style={{width: '100%', height: window.innerHeight}}>
+      
+       
 
-            <div  className="bg-blue-300 w-full h-60 rounded-lg ">
-            <div className="text-center pt-24">
-                
-                <select name="category" className="select select-bordered w-1/2 mx-auto" required >
-                <open>Please Selected Category</open>
-                <option>OnSite Jobs</option>
-                <option>Remote Job</option>
-                <option>Hybrid Jobs</option>
-                <option>Part Time</option>
-            
-            </select>
-                     
-                    </div>
-                
-            </div>
+      <PageTitle  title="Applied jobs Page"/>
+  
+      <div   className="bg-blue-300 w-full h-60 rounded-lg ">
+      <div className="text-center pt-24">
+          
+          <select name="category" className="select select-bordered w-1/2 mx-auto" required >
+          <open>Please Selected Category</open>
+          <option>OnSite Jobs</option>
+          <option>Remote Job</option>
+          <option>Hybrid Jobs</option>
+          <option>Part Time</option>
+      
+      </select>
+               
+              </div>
+          
+      </div>
 
-    
+
 {/* // Table  */}
 
 {
-  applies.length > 0 ? 
-  <div className="bg-zinc-200 my-10" >
+applies.length > 0 ? 
+<div className="bg-zinc-200 my-10" >
 
-  <div className="overflow-x-auto ">
-    <table className="table">
-      {/* head */}
-      <thead >
-        <tr className="text-xl font-bold">
-          <th>
-            <h1>idx</h1>
-          </th>
-          <th>Image</th>
-          <th>Title</th>
-          <th>Email</th>
-          <th>Sallary</th>
-          <th> Action</th>
-        </tr>
-      </thead>
-      <tbody>
-       
-    {
-     applies?.map((apply,idx) => <CardTable key={apply?._id} apply={apply} idx={idx} ></CardTable> )
-    }
-  
-      </tbody>
-   
-    </table>
-  </div>
-   </div> : <div> <h1 className="text-center py-10 text-3xl font-bold">Please Data Add </h1>  </div>
+<div className="overflow-x-auto ">
+<table className="table">
+{/* head */}
+<thead >
+  <tr className="text-xl font-bold">
+    <th>
+      <h1>idx</h1>
+    </th>
+    <th>Image</th>
+    <th>Title</th>
+    <th>Email</th>
+    <th>Sallary</th>
+    <th> Action</th>
+  </tr>
+</thead>
+<tbody>
+ 
+{
+applies?.map((apply,idx) => <CardTable key={apply?._id} apply={apply} idx={idx} ></CardTable> )
 }
 
+</tbody>
 
+</table>
+</div>
+</div> : <div> <h1 className="text-center py-10 text-3xl font-bold">Please Data Add </h1>  </div>
+}
 
+ </div>
 
-
-
-
-        </div>
+ <div>
+   <button className="btn btn-success  capitalize" onClick={handlerPrint} >download summary</button>
+ </div>
+     
+     </>
+      
     );
 };
 
